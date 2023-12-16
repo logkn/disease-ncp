6b90e72bed261099658ceb78c3a88645dc9b2df9
lhk62 <lhk62@cornell.edu>
2023-12-16 04:39:03 -0500

    set up tuner

--- src/ncp/ncp_config.py
+++ src/ncp/ncp_config.py
@@ -9,12 +9,6 @@ class DecoderNCPConfig(TypedDict):
     n_units: int
 
 
-class EncoderDecoderConfig(TypedDict):
-    encoder_n_units: int
-    decoder_n_units: int
-    latent_dim: int
-
-
 class DataConfig(TypedDict):
     data_path: str
     validation_pct: float
@@ -24,23 +18,22 @@ class DataConfig(TypedDict):
 
 
 class NCPTrainingConfig(TypedDict):
-    model_config: EncoderDecoderConfig
-    data_config: DataConfig
+    latent_dim: int
     learning_rate: float
     sigma_lvl: Optional[float]
+    tuning: bool
 
 
 DEFAULT_NCP_TRAINING_CONFIG = NCPTrainingConfig(
-    model_config=EncoderDecoderConfig(
-        encoder_n_units=272, decoder_n_units=272, latent_dim=256
-    ),
+    latent_dim=2,
     data_config=DataConfig(
         data_path="data/synthetic_data",
-        validation_pct=0.2,
-        batch_size=512,
+        validation_pct=0.3,
+        batch_size=2048*4,
         train_samples=50000,
         val_samples=5000,
     ),
-    learning_rate=0.008,
+    learning_rate=0.005,
+    tuning=True,
     # sigma_lvl=0.5,
 )
--- src/ncp/ncp_model.py
+++ src/ncp/ncp_model.py
@@ -4,6 +4,7 @@ from typing import Any, Tuple
 
 import matplotlib.pyplot as plt
 import numpy as np
+import optuna
 import pandas as pd
 import pytorch_lightning as pl
 from pytorch_lightning.utilities.types import EVAL_DATALOADERS, TRAIN_DATALOADERS
@@ -13,19 +14,36 @@ import torch.nn as nn
 from ncp_config import DEFAULT_NCP_TRAINING_CONFIG, NCPTrainingConfig
 from ncps.torch import CfC
 from ncps.wirings import AutoNCP
-from pytorch_lightning.callbacks import DeviceStatsMonitor, EarlyStopping, BatchSizeFinder, RichProgressBar, LearningRateFinder, StochasticWeightAveraging
+from pytorch_lightning.callbacks import (
+    DeviceStatsMonitor,
+    EarlyStopping,
+    BatchSizeFinder,
+    RichProgressBar,
+    LearningRateFinder,
+    StochasticWeightAveraging,
+)
 from torch.nn.utils.rnn import pad_sequence
 from torch.utils.data import DataLoader, TensorDataset
 from tqdm import tqdm
+from optuna.integration import PyTorchLightningPruningCallback
 
-torch.set_float32_matmul_precision('medium')
+pl.seed_everything(42)
+
+torch.set_float32_matmul_precision("medium")
+
+
+class OptunaPruning(PyTorchLightningPruningCallback, pl.Callback):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
 
 class Encoder(nn.Module):
-    def __init__(self, input_size, latent_dim, n_units):
+    def __init__(self, latent_dim):
         super(Encoder, self).__init__()
         # Define AutoNCP wiring for the encoder
-        encoder_wiring = AutoNCP(n_units, latent_dim)
-        self.rnn = CfC(input_size, encoder_wiring)
+        encoder_wiring = AutoNCP(latent_dim, int(0.3 * latent_dim))
+        self.latent_dim = latent_dim
+        self.rnn = CfC(2, encoder_wiring)
 
     def forward(self, x, dt):
         combined_input = torch.cat((x, dt), dim=-1)
@@ -34,24 +52,23 @@ class Encoder(nn.Module):
 
 
 class Decoder(nn.Module):
-    def __init__(self, input_size, hidden_size, output_size, n_units):
+    def __init__(self, hidden_size, output_size):
         super(Decoder, self).__init__()
         # Define AutoNCP wiring for the decoder
-        decoder_wiring = AutoNCP(n_units, hidden_size)
-        self.rnn = CfC(input_size, decoder_wiring)
-        self.output_layer = nn.Linear(hidden_size, output_size)
+        decoder_wiring = AutoNCP(hidden_size, int(0.3 * hidden_size))
+        self.rnn = CfC(1, decoder_wiring, proj_size=1)
 
     def forward(self, hidden_state, dt):
         decoder_output, _ = self.rnn(dt, hx=hidden_state)
-        prediction = self.output_layer(decoder_output)
-        return prediction
+        return decoder_output
 
 
 class EncoderDecoder(pl.LightningModule):
-    def __init__(self, config: NCPTrainingConfig):
+    def __init__(self, config: NCPTrainingConfig, trial: optuna.trial.Trial = None):
         super().__init__()
-        self.model_config = config["model_config"]
         self.config = config
+        self.tuning = config["tuning"]
+        self.trial = trial
 
         self._init_hyperparameters()
 
@@ -61,36 +78,75 @@ class EncoderDecoder(pl.LightningModule):
 
         self._load_trainer()
 
+        self.save_hyperparameters()
+
+    def _objective(trial: optuna.trial.Trial) -> float:
+        """
+        Objective function for Optuna to minimize.
+
+        :param trial: Optuna trial.
+        :return: Validation loss.
+        """
+        config = DEFAULT_NCP_TRAINING_CONFIG
+
+        config["latent_dim"] = trial.suggest_int("latent_dim", 24, 1024)
+        config["learning_rate"] = trial.suggest_float("learning_rate", 1e-5, 1e-1)
+        config["data_config"]["batch_size"] = trial.suggest_int(
+            "batch_size", 128, 2048 * 4
+        )
+
+        config["tuning"] = True
+
+        enc_dec = EncoderDecoder(config, trial)
+        enc_dec.training_loop()
+        return enc_dec.trainer.callback_metrics["val_loss"].item()
+
+    def start_optuna_study(n_trials: int = 100):
+        study = optuna.create_study(
+            direction="minimize",
+            pruner=optuna.pruners.MedianPruner(),
+            study_name="hyperparam_tuning",
+        )
+        study.optimize(EncoderDecoder._objective, n_trials=n_trials, timeout=60*6, show_progress_bar=True)
+        return study
+
     def _load_trainer(self):
         """
         Initializes the PyTorch Lightning trainer.
         """
+        trainer_callbacks = [
+            StochasticWeightAveraging(swa_lrs=1e-3),
+            RichProgressBar(),
+        ]
+
+        if self.tuning:
+            trainer_callbacks.append(OptunaPruning(self.trial, monitor="val_loss"))
+        else:
+            trainer_callbacks.append(EarlyStopping(monitor="val_loss", patience=5))
+
         self.trainer = pl.Trainer(
             logger=[
                 pl.loggers.TensorBoardLogger("lightning_logs/"),
                 pl.loggers.CSVLogger("lightning_logs/"),
             ],
             precision="bf16-mixed",
-            accelerator="gpu",
-            log_every_n_steps=1,
-            callbacks=[
-                EarlyStopping(monitor="val_loss"),
-                BatchSizeFinder(),
-                RichProgressBar(),
-                LearningRateFinder(),
-                StochasticWeightAveraging(swa_lrs=1e-2),
-            ],
+            max_epochs=25,
+            gradient_clip_val=1,
+            log_every_n_steps=5,
+            callbacks=trainer_callbacks,
         )
 
     def training_loop(self):
         self.trainer.fit(
-            self
+            self,
+            train_dataloaders=self.train_dataloader(),
+            val_dataloaders=self.val_dataloader(),
         )
 
     def train_dataloader(self) -> TRAIN_DATALOADERS:
         self._load_datasets()
         return self.train_dl
-    
+
     def val_dataloader(self) -> EVAL_DATALOADERS:
         self._load_datasets()
         return self.val_dl
@@ -99,33 +155,34 @@ class EncoderDecoder(pl.LightningModule):
         """
         Initializes the encoder and decoder models.
         """
-        self.latent_dim = self.model_config["latent_dim"]
 
-        input_size = 2
-        self.latent_dim = self.model_config["latent_dim"]
-        output_size = 1
-
-        n_units_encoder = self.model_config["encoder_n_units"]
-        n_units_decoder = self.model_config["decoder_n_units"]
-        self.learning_rate = self.config["learning_rate"]
+        self.latent_dim = self.config["latent_dim"]
 
         self.encoder = Encoder(
-            input_size, self.latent_dim, n_units_encoder
+            self.latent_dim,
         )  # Adjust sizes as needed
-        self.decoder = Decoder(
-            1, self.latent_dim, output_size, n_units_decoder
-        )  # Assuming dt is 1D
+        self.decoder = Decoder(self.latent_dim, 1)  # Assuming dt is 1D
 
     def _init_hyperparameters(self):
         """
         Initializes the hyperparameters from the config file.
         """
-        self.learning_rate = self.config["learning_rate"]
-        self.validation_pct = self.config["data_config"]["validation_pct"]
+
+        self.hparams["lr"] = self.hparams[
+            "learning_rate"
+        ] = self.learning_rate = self.config["learning_rate"]
         self.data_path = self.config["data_config"]["data_path"]
-        self.batch_size = self.config["data_config"]["batch_size"]
+
+        self.hparams["validation_pct"] = self.validation_pct = self.config[
+            "data_config"
+        ]["validation_pct"]
+        self.hparams["batch_size"] = self.batch_size = self.config["data_config"][
+            "batch_size"
+        ]
+
         self.train_samples = self.config["data_config"]["train_samples"]
         self.val_samples = self.config["data_config"]["val_samples"]
+
         self.sigma_lvl = self.config.get("sigma_lvl", None)
 
     def forward(self, x_encoder, dt_encoder, dt_decoder):
@@ -291,27 +348,28 @@ class EncoderDecoder(pl.LightningModule):
         )
 
         train_dataloader, val_dataloader = DataLoader(
-            train_dataset, batch_size=self.batch_size, shuffle=True
-        ), DataLoader(val_dataset, batch_size=self.batch_size)
+            train_dataset,
+            batch_size=self.batch_size,
+            shuffle=True,
+            num_workers=8,
+            persistent_workers=True,
+        ), DataLoader(
+            val_dataset,
+            batch_size=self.batch_size,
+            num_workers=8,
+            persistent_workers=True,
+        )
 
         self.train_dl, self.val_dl = train_dataloader, val_dataloader
 
     def predict_step(self, batch, batch_idx, dataloader_idx=0):
         return self(batch)
-        
+
 
 if __name__ == "__main__":
-    enc_dec = EncoderDecoder(DEFAULT_NCP_TRAINING_CONFIG)
+    study = EncoderDecoder.start_optuna_study(n_trials=50)
 
-    # # test data
-    # x_encoder = torch.randn(64, 50, 1)
-    # dt_encoder = torch.randn(64, 50, 1)
-    # dt_decoder = torch.randn(64, 100, 1)
-    # y = torch.randn(64, 100, 1)
-
-    # # test forward pass
-    # y_pred = enc_dec(x_encoder, dt_encoder, dt_decoder)
-    # print(y_pred.shape)
-
-    # test training loop
-    enc_dec.training_loop()
+    # Save study
+    study_name = "hyperparam_tuning"
+    study_path = os.path.join("data", study_name)
+    study.trials_dataframe().to_csv(os.path.join(study_path, "trials.csv"))
